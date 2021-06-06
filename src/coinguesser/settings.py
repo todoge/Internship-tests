84fdc451627014d9ad12ab5bb816cf31bbba0469
Peh Jun Siang <austin9090@hotmail.com>
2021-06-06 15:35:38 +0800

    Try deploying to heroku

--- src/coinguesser/settings.py
+++ src/coinguesser/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 from django.db import models
 
@@ -141,3 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+django_heroku.settings(locals())
