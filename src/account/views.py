2f740f5bfc12651f777768d8b4fcc82aa0a2ce16
Peh Jun Siang <austin9090@hotmail.com>
2021-06-06 15:21:29 +0800

    Update user auth

--- src/account/views.py
+++ src/account/views.py
@@ -1,17 +1,18 @@
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import redirect, render
-from .forms import CreateUserForm
+# from .forms import CreateUserForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 
 # Create your views here.
 def registerPage(request):
-    form = CreateUserForm()
+    form = UserCreationForm()
     if request.user.is_authenticated:
         return redirect("/")
     else:
         if request.method == "POST":
-            form = CreateUserForm(request.POST)
+            form = UserCreationForm(request.POST)
             if form.is_valid():
                 form.save()
                 print(form)
@@ -19,7 +20,7 @@ def registerPage(request):
                 user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
                 login(request, user)
                 return HttpResponseRedirect("/")
-            
+
     context = {
         'form': form,
     }
